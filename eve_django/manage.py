32cff93e6786dc6c04069ce44add4870892b035f
Thomas Neyland <instanceoftom@gmail.com>
2013-06-29 00:48:55 +0000

    typo

--- eve_django/manage.py
+++ eve_django/manage.py
@@ -3,7 +3,7 @@ import os
 import sys
 
 if __name__ == "__main__":
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "wheel.settings")
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "eve_django.settings")
 
     from django.core.management import execute_from_command_line
 
