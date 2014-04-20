1897867f2db96cc24ba76a4cb7ef3a7c373b4289
Tom Neyland <tcneyland+github@gmail.com>
2014-04-20 19:45:31 -0400

    updates download script

--- eve_django/download-sde-db.sh
+++ eve_django/download-sde-db.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 FILE_URL=$1
 OUT_FILE=$2
-: ${FILE_URL:="https://www.fuzzwork.co.uk/dump/odyssey-1.0.7-89602/odyssey107.sqlite.bz2"}
+: ${FILE_URL:="https://www.fuzzwork.co.uk/dump/sqlite-latest.sqlite.bz2"}
 : ${OUT_FILE:="eve.db"}
 echo "Downloading $FILE_URL to $OUT_FILE with command:"
 echo "wget -q $FILE_URL -O - | bzip2 -cd > $OUT_FILE"
